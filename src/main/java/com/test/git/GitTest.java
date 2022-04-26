db33f1d720afa18a1905d832d33a34c811baae04
Morceleb <2283528477@qq.com>
2022-04-26 19:55:47 +0800

    second commit

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -1,11 +1,18 @@
 package com.test.git;
 
 public class GitTest {
+    static class LuXF{
+        public void ningXiZhi(){
+            System.out.println("使用了灵犀指");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello GitHub!");
         System.out.println("HuaQ! the great firewall!");
+        LuXF lxj = new LuXF();
+        lxj.ningXiZhi();
         //测试git用文档
-	//我都觉得害怕
+	    //我都觉得害怕
     }
 }
